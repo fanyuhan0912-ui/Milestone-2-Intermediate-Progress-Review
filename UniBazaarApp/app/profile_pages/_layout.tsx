0dc112f7fdb8b88cbc7882a6f03c7bb30e08b196
Carolyn Fan <fanyuhan0912@gmail.com>
2025-12-02 10:14:54 -0800

    .

--- UniBazaarApp/app/profile_pages/_layout.tsx
+++ UniBazaarApp/app/profile_pages/_layout.tsx
@@ -5,12 +5,17 @@ export default function ProfilePagesLayout() {
   return (
     <Stack>
       <Stack.Screen name="favourite" options={{ headerShown: false }} />
-      <Stack.Screen name="listed" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="listed"
+        options={{ headerShown: true, title: 'Listed Items' }} 
+      />
+
       <Stack.Screen name="purchased" options={{ headerShown: false }} />
       <Stack.Screen name="sold" options={{ headerShown: false }} />
       <Stack.Screen name="toReview" options={{ headerShown: false }} />
       <Stack.Screen name="userHomepage" options={{ headerShown: false }} />
       <Stack.Screen name="viewMore" options={{ headerShown: false }} />
+    
     </Stack>
   );
 }
